52eb2f6b6a54f8f3763556b3aca1f7a37e4afa93
azinoreva <azinoreva@gmail.com>
2024-09-21 16:31:38 -0700

    done with basic auth

--- 0x01-Basic_authentication/SimpleAPI/api/v1/views/index.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/views/index.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
-""" Module of Index views
-"""
+""" Module of Index views """
 from flask import jsonify, abort
 from api.v1.views import app_views
-
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
@@ -13,20 +11,25 @@ def status() -> str:
     """
     return jsonify({"status": "OK"})
 
-
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
     """ GET /api/v1/stats
     Return:
-      - the number of each objects
+      - the number of each object
     """
     from models.user import User
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
 
-
-@app_views.route('/api/v1/unauthorized', methods=['GET'], strict_slashes=False)
-def unauthorized_access() ->str:
-    """ Endpoint for 401 raise """
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized_access() -> str:
+    """ Endpoint that raises 401 Unauthorized error """
     abort(401)
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """ GET /api/v1/forbidden
+    Return - raises 403 error
+    """
+    abort(403)
